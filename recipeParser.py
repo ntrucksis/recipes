9ab1802070dd527f0f1dd3cbb24a789cb17b3ec1
Nick Trucksis <nicholastrucksis2020@u.northwestern.edu>
2020-02-17 15:17:31 -0600

    preparation and descriptors, and takes Url input

--- recipeParser.py
+++ recipeParser.py
@@ -2,8 +2,10 @@ from bs4 import BeautifulSoup
 import json
 import requests
 from nltk import pos_tag, word_tokenize
+import sys
 
-requestUrl =  "https://www.allrecipes.com/recipe/175383/shrimp-scampi-with-linguini/?internalSource=previously%20viewed&referringContentType=Homepage&clickId=cardslot%204"
+requestUrl = input("Enter the Url of Recipe Page: ")
+# requestUrl =  "https://www.allrecipes.com/recipe/175383/shrimp-scampi-with-linguini/?internalSource=previously%20viewed&referringContentType=Homepage&clickId=cardslot%204"
 
 response = requests.get(requestUrl)
 
@@ -11,9 +13,7 @@ response = requests.get(requestUrl)
 soup = BeautifulSoup(response.text, features="lxml")
 
 # access & format the recipe title
-recipeTitle = soup.title.string
-recipeTitle = recipeTitle.split('-')
-recipeTitle = recipeTitle[0]
+recipeTitle = soup.title.string.split('-')[0]
 
 # access & format the ingredients list
 ingredients = soup.find_all("li", class_="checkList__line")
@@ -28,34 +28,49 @@ ingredientsResult = ingredientsResult[:len(ingredientsResult)-3]
 name = []
 quantity = []
 measurement = []
+preparation = []
+descriptor = []
 
 for indx in range(len(ingredientsResult)):
     tokenized = pos_tag(word_tokenize(ingredientsResult[indx]))
     for word in tokenized:
         if word[1] == 'CD':
             quantity.append(word[0])
-        if word[1] == 'NN':
+        elif word[1] == 'NN':
             if word[0] not in ['package', 'cup', 'teaspoon', 'tablespoon', 'ounce']:
                 name.append(word[0])
             else:
                 measurement.append(word[0])
+        elif word[1] == 'VBD':
+            preparation.append(word[0])
+        elif word[1] == 'JJ':
+            descriptor.append(word[0])
 
 # http response code for debugging
 # print(response.status_code)
+
+# tkn = pos_tag(word_tokenize(ingredientsResult[3]))
 
 print(recipeTitle)
 
 print("\nLIST OF INGREDIENTS FOR RECIPE")
 
 for i in range(len(ingredientsResult)):
-     print(ingredientsResult[i])
+    print(ingredientsResult[i])
 
-print("NAMES")
+print("\nNAMES")
 print(name)
 
-print("QUANTITIES")
+print("\nQUANTITIES")
 print(quantity)
  
-print("MEASUREMENTS")
+print("\nMEASUREMENTS")
 print(measurement)
 
+print("\nPREPERATIONS")
+print(preparation)
+
+print("\nDESCRIPTORS")
+print(descriptor)
+
+# print(tkn)
