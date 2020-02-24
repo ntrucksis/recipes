eb6e142741007e905aa19a1b7440513df5dd3f7d
Nick Trucksis <nicholastrucksis2020@u.northwestern.edu>
2020-02-24 14:42:43 -0600

    adjusted ingredient lists

--- recipeParser.py
+++ recipeParser.py
@@ -23,7 +23,7 @@ def getIngredientsObject(ingredientsList):
                 q.append(word[0])
                 quant += word[0] + " "
             elif word[1] in ['JJ', 'MD', 'VBZ', 'RB']:
-                if word[0] in ['black', 'olive', 'maple', 'green',  'red', 'white', 'beef', 'garlic', 'sour', 'lemon', 'heavy', 'large', 'yellow', 'chocolate', 'vegetable', 'lime', 'angel', 'bread', 'cheese']:
+                if word[0] in ['black', 'olive', 'maple', 'beef', 'garlic', 'sour', 'lemon', 'heavy', 'large', 'yellow', 'chocolate', 'vegetable', 'lime', 'angel', 'bread', 'cheese', 'chorizo', 'chipotle']:
                     name += word[0] + " "
                 else:
                     if word[0] in ['pinch', 'cup', 'can', 'cans', 'packages', 'fluid', 'squares', 'teaspoon']:
@@ -37,7 +37,7 @@ def getIngredientsObject(ingredientsList):
                     else:
                         desc += word[0] + " "
             elif word[1] in ['NN', 'NNS', 'NNP']:
-                if word[0] not in ['package', 'cup', 'teaspoon', 'tablespoon', 'ounce', 'teaspoons', 'pound', 'pounds', 'tablespoons', 'pint', 'pinch', 'cups', 'ounces', 'slices', 'packages', 'cloves', 'frying', 'drop', 'packet', 'fluid', 'head']:
+                if word[0] not in ['package', 'cup', 'teaspoon', 'tablespoon', 'ounce', 'teaspoons', 'pound', 'pounds', 'tablespoons', 'pint', 'pinch', 'cups', 'ounces', 'slices', 'packages', 'cloves', 'frying', 'drop', 'packet', 'fluid', 'head', 'inch']:
                     if word[0] in ['ground', 'pieces', 'room', 'temperature', 'chunks', 'florets']:
                         prep += word[0] + " "
                     elif word[0] in ['Pillsbury®', 'Recipe', 'Creations®', 'Campbell\'s®']:
@@ -66,10 +66,15 @@ def getIngredientsObject(ingredientsList):
                 pass
 
         if len(q) > 1:
-            if q[1] not in ['1/4', '1/2', '3/4', '1/3', '2/3']:
-                msmtPart = q[1] + " "
-                quant = quant[:2]
-                msmt = msmtPart + msmt
+            tmp = q[1:]
+            print(tmp)
+            for element in tmp:
+                if element not in ['1/4', '1/2', '3/4', '1/3', '2/3']:
+                    msmtPart = element + " "
+                    quant = quant[:2]
+                    msmt = msmtPart + msmt
+                else:
+                    pass
 
         name = name[:-1]
         quant = quant[:-1]
